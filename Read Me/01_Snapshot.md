13efae44c738ebe0082e64dd4c9f5544f2a82a46
Acet-Aminophen <leeds06080@gmail.com>
2022-11-11 13:21:20 +0900

    commit legacy version

---
+++ Read Me/01_Snapshot.md
@@ -0,0 +1,74 @@
+01_Snapshot
+=============
+
+본 문서는 KF2 서버가 업데이트되었을 때 GCP Snapshot을 바꾸는 방법을 서술한다.  
+
+1. 아래와 같은 사양의 인스턴스를 만들되, 30GB의 추가 디스크를 생성한다.  
+```
+e2-medium
+Ubuntu 20.04
+asia-northeast3(서울)
+10GB
+```
+
+2. SSH로 접근 후 다음과 같은 절차로 Steam CMD를 설치한다.<sup>[[1]](#footnote_1)</sup>
+
+```
+# 전체 복사 붙여넣기 가능
+sudo apt update
+sudo add-apt-repository multiverse
+sudo apt install software-properties-common
+sudo dpkg --add-architecture i386
+sudo apt update
+sudo apt install lib32gcc-s1 steamcmd
+```
+
+3. 추가 디스크를 초기화 및 마운트한다.<sup>[[2]](#footnote_2)</sup>
+```
+# 디스크 확인
+sudo lsblk
+# 초기화
+sudo mkfs.ext4 -m 0 -E lazy_itable_init=0,lazy_journal_init=0,discard /dev/sdb
+```
+```
+# 마운트
+sudo mkdir -p /home/steam/server
+sudo mount -o discard,defaults /dev/sdb /home/steam/server
+sudo chmod a+w /home/steam/server
+```
+
+4. Steam CMD로 서버를 설치한다.
+```
+steamcmd
+force_install_dir /home/steam/server
+login anonymous
+app_update 232130 validate
+quit
+```
+
+5. 다음의 파일을 지시와 같이 수정한다.
+```
+# 웹 어드민 활성화
+# /home/steam/server/KFGame/Config/DefaultWeb.ini
+# [IpDrv.WebServer]
+bEnabled=true
+```
+```
+# 서버 강탈 비활성화
+# /home/steam/server/KFGame/Config/DefaultEngine.ini
+# [Engine.GameEngine]
+bUsedForTakeover=FALSE
+```
+
+6. '/home/steam/server/KFGame/Config/DefaultGame.ini' 파일을 다운로드한다.<sup>[[3]](#footnote_3)</sup>
+
+7. 디스크를 스냅샷으로 저장한다.
+
+
+
+
+<a name="footnote_1">[1]</a>: 출처 : https://wiki.killingfloor2.com/index.php?title=Dedicated_Server_(Killing_Floor_2), https://developer.valvesoftware.com/wiki/SteamCMD#Downloading_SteamCMD
+
+<a name="footnote_2">[2] </a>: 출처 : https://cloud.google.com/compute/docs/disks/add-persistent-disk?hl=ko
+
+<a name="footnote_3">[3] </a>: Router에서 보관하여 이름, 비밀번호를 만들 때 사용
---
+++ main.py
@@ -0,0 +1,271 @@
+from shutil import copyfile
+from BasicPackage.BasicFunc import *
+import GCEConnector
+import discord
+import logging
+import logging.config
+import time
+import random
+import threading
+import os
+
+logging.config.fileConfig("logging.cfg")
+config_path = "config/config.cfg"
+
+CONF_FILE_SERVER_IP = get_config(config_path, "CONF_FILE_SERVER_IP")
+CONF_FILE_SERVER_PORT = get_config(config_path, "CONF_FILE_SERVER_PORT")
+CONF_FILE_SERVER_USER = get_config(config_path, "CONF_FILE_SERVER_USER")
+CONF_FILE_SERVER_PWD = get_config(config_path, "CONF_FILE_SERVER_PWD")
+CONF_FILE_PATH_DEFAULT_GAME = get_config(config_path, "CONF_FILE_PATH_DEFAULT_GAME")
+PROJECT_ABSOLUTE_PATH = get_config(config_path, "PROJECT_ABSOLUTE_PATH")
+
+DISCORD_SERVER_ID: int = int(get_config(config_path, "DISCORD_SERVER_ID"))
+DISCORD_REQUEST_CHANNEL_ID = int(get_config(config_path, "DISCORD_REQUEST_CHANNEL_ID"))
+BOT_TOKEN = get_config(config_path, "BOT_TOKEN")
+BOT_ID = int(get_config(config_path, "BOT_ID"))
+TMP_DEV_IDS = get_config(config_path, "DEV_IDS")
+DEV_ID_LIST = []
+for i in TMP_DEV_IDS:
+    DEV_ID_LIST.append(int(i))
+GCE_ID = get_config(config_path, "GCE_ID")
+GCE_PW = get_config(config_path, "GCE_PW")
+GCE_PR = get_config(config_path, "GCE_PR")
+SERVER_EXPIRE_SEC = int(get_config(config_path, "SERVER_EXPIRE_SEC"))
+SERVER_REQUEST_DURATION_SEC = int(get_config(config_path, "SERVER_REQUEST_DURATION_SEC"))
+REFRESH_SEC = int(get_config(config_path, "REFRESH_SEC"))
+SERVER_BLOCK_REQUEST_SEC = int(get_config(config_path, "SERVER_BLOCK_REQUEST_SEC"))
+
+gce_driver = GCEConnector.get_driver(GCE_ID, GCE_PW, GCE_PR)
+intents = discord.Intents.all()
+client = discord.Client(intents=intents)
+
+user_dic = {}
+flag_server_creating: bool = False
+server_list: list = []
+latest_server_created: int = int(time.time()) - 10800
+
+logging.info("--------------------------------------------------------------")
+logging.info("SERVER IS BOOTING...")
+logging.info("PID : " + str(os.getpid()))
+
+
+def get_init_sequence():
+    global CONF_FILE_SERVER_IP
+    global CONF_FILE_SERVER_PORT
+    global CONF_FILE_SERVER_USER
+    global CONF_FILE_SERVER_PWD
+    global PROJECT_ABSOLUTE_PATH
+
+    order_list = ["#! /bin/bash", "sudo mkdir /orgserver", "sudo mount -o discard,defaults /dev/sdb /orgserver",
+                  "sudo apt update", "sudo apt install sshpass",
+                  "sudo sshpass -p '" + CONF_FILE_SERVER_PWD + "' scp -P " + CONF_FILE_SERVER_PORT + " -o StrictHostKeyChecking=no " + CONF_FILE_SERVER_USER + "@" + CONF_FILE_SERVER_IP + ":" + PROJECT_ABSOLUTE_PATH + CONF_FILE_PATH_DEFAULT_GAME + " /orgserver/KFGame/Config/DefaultGame.ini",
+                  "cd /", "./orgserver/Binaries/Win64/KFGameSteamServer.bin.x86_64 kf-bioticslab"]
+
+    start_up_str = ""
+    for order in order_list:
+        start_up_str += order + "\n"
+
+    return start_up_str
+
+
+def get_hour_from_sec(sec: int):
+    return round(sec / 3600, 1)
+
+
+def alert(content: str):
+    global client
+    global DEV_ID_LIST
+    for id_is in DEV_ID_LIST:
+        client.loop.create_task(send_dm(id, content))
+
+
+def chk_status():
+    locker = threading.Lock()
+    locker.acquire()
+    global server_list
+    global gce_driver
+    global latest_server_created
+    global client
+    global SERVER_EXPIRE_SEC
+    global REFRESH_SEC
+    logging.info("Server List ↓")
+    logging.info(server_list)
+    logging.info("Latest Created ↓")
+    logging.info(latest_server_created)
+    # 복사본으로 진행하여 remove를 하게함
+    for i in server_list[:]:
+        created_time: int = i.get("created_time")
+        expired_time: int = i.get("expired_time")
+        if created_time + expired_time < int(time.time()):
+            logging.info("Destroy node ↓")
+            logging.info(i)
+            gce_driver.destroy_node(i.get("node"))
+            message_from = i.get("message_from")
+            client.loop.create_task(send_dm(message_from, "요청하셨던 서버의 유효시간이 지나 종료되었습니다."))
+            server_list.remove(i)
+    locker.release()
+    threading.Timer(REFRESH_SEC, chk_status).start()
+
+
+def get_config_path(uuid: str):
+    temp_conf_location = "game_config/temp/" + uuid + "/"
+    temp_conf_location_default_game = temp_conf_location + "DefaultGame.ini"
+    return temp_conf_location, temp_conf_location_default_game
+
+
+def chg_game_config(name: str, pwd: str, uuid: str):
+    global CONF_FILE_PATH_DEFAULT_GAME
+
+    temp_conf_location, temp_conf_location_default_game = get_config_path(
+        uuid)
+    make_directory(temp_conf_location)
+    copyfile(CONF_FILE_PATH_DEFAULT_GAME, temp_conf_location_default_game)
+    server_name = "KF2SAM|" + name
+
+    str_org = ""
+    reader = open(temp_conf_location_default_game, 'r', encoding="utf-8")
+    while True:
+        line = reader.readline()
+        if not line:
+            break
+        if "ServerName=" in line:
+            line = "ServerName=" + server_name + "\n"
+        if "AdminPassword=" in line:
+            if pwd != "":
+                line = "AdminPassword=" + pwd + "\n"
+            else:
+                line = "AdminPassword=" + "\n"
+        if "GamePassword=" in line:
+            if pwd != "":
+                line = "GamePassword=" + pwd + "\n"
+            else:
+                line = "GamePassword=" + "\n"
+        str_org += line
+    reader.close()
+    writer = open(temp_conf_location_default_game, 'w', encoding="utf-8")
+    writer.write(str_org)
+    writer.close()
+    return server_name
+
+
+def start_gcp_server(name: str, pwd: str):
+    global gce_driver
+    global SERVER_EXPIRE_SEC
+
+    uuid = get_uuid()
+    server_name = chg_game_config(name, pwd, uuid)
+    metadata = {
+        'items': [
+            {
+                'key': 'startup-script',
+                'value': get_init_sequence()
+            }
+        ]
+    }
+    volume_name = "vol-" + name
+    node_name = "nod-" + name
+    vol = gce_driver.create_volume(size=30, name=volume_name, location="asia-northeast3-a", snapshot="kf2-server-origin-snp-220710")
+    node = gce_driver.create_node(name=node_name, size="e2-medium", image="ubuntu-18", location="asia-northeast3-a", ex_disk_size=10, ex_metadata=metadata)
+    gce_driver.attach_volume(node, vol, ex_mode="READ_WRITE", ex_auto_delete=True)
+    time_now = int(time.time())
+    return {"name": name, "password": pwd, "server_name": server_name, "node_name": node_name,
+            "volume_name": volume_name,
+            "created_time": time_now, "expired_time": SERVER_EXPIRE_SEC, "ip": node.public_ips[0], "node": node}
+
+
+@client.event
+async def on_ready():
+    logging.info("BOT IS PREPARED")
+    alert("KF2 SAM BOT ONLINE")
+
+
+@client.event
+async def on_message(message):
+    logging.info([str(message.created_at), str(message.author.id), str(message.guild.id) if message.guild else "",
+                  str(message.channel.id) if message.guild else "", str(message.content) if message.content else ""])
+    if not message.content:
+        return
+    # 내용 없을 경우 종료
+    await route_message(message)
+
+
+async def send_dm(id_is, content: str):
+    try:
+        location_to_send = client.get_user(int(id_is))
+        await location_to_send.send(content)
+    except:
+        pass
+    return
+
+
+async def send_channel_message(id_is, content: str):
+    try:
+        location_to_send = client.get_channel(int(id_is))
+        await location_to_send.send(content)
+    except:
+        pass
+    return
+
+
+def is_on_the_channel(message):
+    global DISCORD_REQUEST_CHANNEL_ID
+    if not message.channel:
+        return False
+    if message.channel.id == DISCORD_REQUEST_CHANNEL_ID:
+        return True
+    else:
+        return False
+
+
+def start_server(message):
+    locker = threading.Lock()
+    locker.acquire()
+    global latest_server_created
+    global flag_server_creating
+    flag_server_creating = True
+    server_name = "r" + str(random.randrange(1, 10000))
+    server_pwd = str(random.randrange(1, 10000))
+    dic = start_gcp_server(server_name, server_pwd)
+    dic["message_from"] = message.author.id
+    server_list.append(dic)
+    latest_server_created = int(time.time())
+    flag_server_creating = False
+    msg = "서버 초기화 완료.\n서버 부팅 시작됨.\n대략 3분 후 서버 브라우저에서 아래와 같은 사항으로 확인 가능합니다.\n\n서버 이름 : " + dic.get(
+        "server_name") + "\n서버 비밀번호 : " + dic.get("password") + "\n서버 관리 웹페이지 : http://" + dic.get(
+        "ip") + ":8080/" + "\n서버 관리 웹페이지 ID : admin\n서버 관리 웹페이지 PW : " + dic.get("password")
+    alert(msg)
+    client.loop.create_task(send_dm(message.author.id, msg))
+    locker.release()
+
+
+async def route_server_request(message):
+    global flag_server_creating
+    global latest_server_created
+    global user_dic
+    global SERVER_BLOCK_REQUEST_SEC
+    global SERVER_REQUEST_DURATION_SEC
+
+    author: int = message.author.id
+    user_requested_time = user_dic.get(str(author), 0)
+    if user_requested_time + SERVER_BLOCK_REQUEST_SEC > int(time.time()):
+        await send_channel_message(message.channel.id, "서버 생성 불허.\n최근 서버를 신청한지 6시간 이내입니다.")
+        return
+
+    user_dic[str(author)] = int(time.time())
+    await send_channel_message(message.channel.id, "서버 생성 허가 완료.\n서버 생성 수립 중...\n수립이 완료되면 DM을 보내드리며 이 과정은 5분 정도 걸립니다.")
+    threading.Thread(target=start_server, args=(message,)).start()
+
+
+async def route_message(message):
+    # 채널 메세지 아니면 종료
+    if not is_on_the_channel(message):
+        return
+    content: str = str(message.content)
+    if content == "서버요청":
+        await route_server_request(message)
+    else:
+        return
+
+
+threading.Timer(0, chk_status).start()
+client.run(BOT_TOKEN)
